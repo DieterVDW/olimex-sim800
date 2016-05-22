1e7b190040d8f49c730bce317c53eb9c8b3c33ef
Dieter Van de Walle <dietervdw@gmail.com>
2016-05-22 23:58:19 +0200

    Working auto-setup of GPRS connection + send value. Also added some code to turn on GSM module if needed.

--- SIM800-serial.ino
+++ SIM800-serial.ino
@@ -1,81 +1,92 @@
-#include <SIM800.h>
-
-SIM800Core  core;
-SIM800GSM GSM800;
-
 #define LOG(x) Serial.print(millis()); Serial.print(' '); Serial.println(x);
 
-#define SINGLE_RUN_AND_PIPE
+// #define SINGLE_RUN_AND_PIPE 1
 
-#define SERVER "dietervdw.dyndns.org"
+#define SERVER "dietervdw.ddns.net"
 #define PORT 12345
 
-#define REPORT_DELAY 15000
+#define REPORT_DELAY 30000
 
 #define PIN 6776
 
+#define GSM_POWERSWITCH_PIN 11
+
 void setup()
 {
+  pinMode(GSM_POWERSWITCH_PIN, OUTPUT);
+  digitalWrite(GSM_POWERSWITCH_PIN, LOW); // High by default
+
   Serial.begin(115200);
   while (!Serial);
   Serial.println("Started serial!");
 
-  pinMode(11, OUTPUT);
-
-  digitalWrite(11, LOW);
-
-  delay(1200);
-
-  digitalWrite(11, HIGH);
-  
-  
   Serial1.begin(115200);
   while (!Serial1);
   Serial.println("Started serial1!");
 
-  core.host.echo(false);
+  int gsmModuleActive = 0;
+  for (int i = 0; i < 3; i++) {
+    delay(500);
+    gsmModuleActive += testGSMModuleActive();
+  }
+  if (gsmModuleActive == 0) {
+    turnOnGSMModule();
+  }
 
   waitForGSMModule();
+  sendStringForOK("ATE0"); // Host echo off
 
   delay(1000);
 
-  // checkPIN();
+  checkPIN();
 
-  // setupGPRS();
+  setupGPRS();
 }
 
 void waitForGSMModule() {
-  boolean ok = false;
+  while (!testGSMModuleActive()) {
+    delay(1000);
+  }
+  LOG("GSM Module OK!");
+}
 
-  while (!false) {
-    LOG("Testing GSM module presence ...");
-    sendString("AT");
-    delay(500);
-    if (Serial1.available() > 0) {
-      String response = readString();
-      if (response == "OK") {
-        break;
-      }
-    } else {
-      delay(1000);
+void turnOnGSMModule() {
+  LOG("Trying to turn on GSM module!");
+  digitalWrite(GSM_POWERSWITCH_PIN, HIGH);
+  delay(1200);
+  digitalWrite(GSM_POWERSWITCH_PIN, LOW); // High by default
+}
+
+void turnOffGSMModule() {
+  LOG("Shutting down GSM module!");
+  sendStringForOK("AT+CPOWD=1");
+}
+
+int testGSMModuleActive() {
+  LOG("Testing GSM module presence ...");
+  sendString("AT");
+  delay(1000);
+  if (Serial1.available() > 0) {
+    String response = readString();
+    if (response == "OK" || response == "ATOK") {
+      return 1;
     }
   }
-  Serial.println("GSM Module OK!");
+  return 0;
 }
 
 void checkPIN() {
-  sendString("AT+CPIN?");
-  String response = readString();
+  String response = sendStringForOK("AT+CPIN?");
 
   if (response.indexOf("READY") == -1) {
     LOG("Entering PIN");
-    sendString("AT+CPIN=" + PIN);
-    response = readString();
+    String command = "AT+CPIN=";
+    response = sendStringForOK(command + PIN);
 
-    sendString("AT+CPIN?");
+    response = sendStringForOK("AT+CPIN?");
     if (response.indexOf("READY") == -1) {
       LOG("Could not verify PIN! Aborting!");
-      abort();
+      ;
     }
   }
 
@@ -84,27 +95,26 @@ void checkPIN() {
 
 void setupGPRS() {
   LOG("Starting GPRS");
-  sendString("AT+CGATT?");
-  String response = readString();
+  String response = sendStringForOK("AT+CGATT?");
   while (response.indexOf('1') == -1) {
     LOG("GPRS not connected, waiting! 1");
     delay(1000);
-    sendString("AT+CGATT?");
-    String response = readString();
+    response = sendStringForOK("AT+CGATT?");
   }
 
   delay(1000);
-  sendString("AT+CIPMUX=0");
-  response = readString();
+
+  stopTCPConnection();
+
+  response = sendStringForOK("AT+CIPMUX=0");
   while (response.indexOf('OK') == -1) {
-    LOG("GPRS not connected, waiting! 1");
+    LOG("GPRS not connected, waiting! 2");
     delay(1000);
-    sendString("AT+CIPMUX=0");
-    response = readString();
+    response = sendStringForOK("AT+CIPMUX=0");
   }
 
-  sendString("AT+CSTT=\“internet.proximus.be\”,\“\”,“\”"); readString();
-  sendString("AT+CIICR"); readString();
+  sendStringForOK("AT+CSTT=\"internet.proximus.be\",\"\",\"\"");
+  sendStringForOK("AT+CIICR");
   sendString("AT+CIFSR");
   String ip = readString();
 
@@ -113,19 +123,21 @@ void setupGPRS() {
 
 void loop ()
 {
-//  LOG("Starting TCP connection...");
-//  startTCPConnection(SERVER, PORT);
-//
-//  int sensorValue = analogRead(0);
-//  Serial.print("Sending value: "); Serial.println(sensorValue);
-//
-//  LOG("Sending sensor value");
-//  sendSensorValue(sensorValue);
-//  LOG("Sensor value sent!");
-//
-//  stopTCPConnection();
-//  LOG("TCP connection shut down!");
-//  Serial.println();
+  LOG("Starting TCP connection...");
+  int ret = startTCPConnection(SERVER, PORT);
+  if (ret >= 0) {
+    int sensorValue = analogRead(0);
+    String msg = "Sending value: ";
+    LOG(msg + sensorValue);
+
+    LOG("Sending sensor value");
+    sendSensorValue(sensorValue);
+    LOG("Sensor value sent!");
+  }
+
+  stopTCPConnection();
+  LOG("TCP connection shut down!");
+  Serial.println();
 
 #ifdef SINGLE_RUN_AND_PIPE
   while (1) {
@@ -136,14 +148,37 @@ void loop ()
   delay(REPORT_DELAY);
 }
 
+String sendStringForOK(String s) {
+  sendString(s);
+  String response = readString();
+  if (response.indexOf("ERROR") >= 0) {
+    LOG("GOT ERROR!!!");
+    ;
+  }
+  while (response.indexOf("OK") == -1) {
+    sendString(s);
+    response = readString();
+  }
+  return response;
+}
+
 void sendString(String s) {
+  LOG("Sending: " + s);
   Serial1.println(s + "\r");
+  Serial1.flush();
+  delay(500);
 }
 
 String readString() {
+  return readString(20);
+}
+
+String readString(int maxWaitTime) {
   String response = String();
-  while (!Serial1.available() > 0) {
+  int waitCount = 0;
+  while (!Serial1.available() > 0 && maxWaitTime > 0) {
     delay(100);
+    maxWaitTime--;
   }
   while (Serial1.available() > 0) {
     char c = Serial1.read();
@@ -151,31 +186,45 @@ String readString() {
       response += c;
     }
   }
-  Serial.print("Got response: ");
-  Serial.println(response);
+  LOG("Got response: " + response);
   return response;
 }
 
-void startTCPConnection(String server, int port) {
-  sendString("AT+CIPSHUT"); readString();
+int startTCPConnection(String server, int port) {
+  sendStringForOK("AT+CIPSHUT");
   delay(1000);
-  sendString("AT+CIPSTART=\”TCP\”,\”" + server + "\”,\”" + String(port) + "\”");
-  String response = readString();
-  while (response.indexOf('CONNECTED') == -1) {
-    LOG("TCP connection not connected, waiting! 1");
+  String response = sendStringForOK("AT+CIPSTART=\"TCP\",\"" + server + "\",\"" + port + "\"");
+  int tries = 0;
+  do {
     delay(1000);
-    sendString("AT+CIPSTART=\”TCP\”,\”" + server + "\”,\”" + String(port) + "\”");
     response = readString();
+    tries++;
+  } while (response.indexOf("CONNECT OK") == -1 && tries < 5);
+  if (tries == 5) {
+    return -1;
   }
-  sendString("AT+CIPSEND"); readString();
+  delay(1000);
+  sendString("AT+CIPSEND");
+  tries = 0;
+  do {
+    delay(1000);
+    response = readString();
+    tries++;
+  } while (response.indexOf(">") == -1 && tries < 5);
+  if (tries == 5) {
+    return -1;
+  }
+  return 0;
 }
 
 void stopTCPConnection() {
-  sendString("\zAT+CIPSHUT"); readString();
+  sendStringForOK("AT+CIPSHUT");
 }
 
 void sendSensorValue(int value) {
   sendString("The value is: " + String(value) + "\r\n");
+  Serial1.write(26);
+  Serial1.flush();
 }
 
 void pipeInOut() {
